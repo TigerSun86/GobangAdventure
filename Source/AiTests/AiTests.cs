340ba20f498b0875e5a72772a8f955722ce60d21
TigerSun <TigerSun86@gmail.com>
2017-04-10 21:02:07 -0700

    -Added AiTests -Added Utils to deserialize a board from string

---
+++ Source/AiTests/AiTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GobangGameLib.GameBoard;
+using GobangGameLib.GameBoard.PositionManagement;
+using GoBangGameLibTest.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AiTests
+{
+    [TestClass]
+    public class AiTests
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var boardString = new[]
+            {
+                "           ",
+                "           ",
+                "           ",
+                "           ",
+                "    XO     ",
+                "    XXO    ",
+                "      OO   ",
+                "      X    ",
+                "           ",
+                "           ",
+                "           ",
+            };
+
+            var context = new BoardProperties();
+            var positions = new PositionFactory().Create(context);
+
+            var result = Utils.ParseBoard(boardString, context, positions);
+        }
+
+    }
+}
---
+++ Source/GoBangGameLibTest/Common/FreeLine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GobangGameLib.GameBoard;
+using GobangGameLib.GameBoard.PositionManagement;
+
+namespace GoBangGameLibTest.Common
+{
+    public class FreeLine: IPositions
+    {
+        private readonly List<Position> _positions;
+
+        public FreeLine(IEnumerable<Position> positions)
+        {
+            _positions = positions.ToList();
+        }
+
+        public IList<Position> Positions
+        {
+            get
+            {
+                return _positions;
+            }
+        }
+    }
+}
