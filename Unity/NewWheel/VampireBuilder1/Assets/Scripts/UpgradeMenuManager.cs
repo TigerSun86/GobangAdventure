3604890810fc54e458607a2a6a8caa4472092ba1
Tiger Sun <tigersun86@gmail.com>
2022-11-27 19:20:23 -0800

    Made upgrade menu can have pending upgrade. When user selected one upgrade, then 2nd pending upgrade will show

--- Unity/NewWheel/VampireBuilder1/Assets/Scripts/UpgradeMenuManager.cs
+++ Unity/NewWheel/VampireBuilder1/Assets/Scripts/UpgradeMenuManager.cs
@@ -6,9 +6,11 @@ using UnityEngine.EventSystems;
 
 public class UpgradeMenuManager : MonoBehaviour
 {
-    [SerializeField] GameObject panel;
+    private readonly object upgradeVariablesLock = new object();
 
-    [SerializeField] bool isEnabled = true;
+    [SerializeField] IntVariable pendingUpgradeCount;
+
+    [SerializeField] GameObject panel;
 
     [SerializeField] SkillRuntimeSet skillPrefabs;
 
@@ -16,13 +18,9 @@ public class UpgradeMenuManager : MonoBehaviour
 
     [SerializeField] List<SkillUpgradeButton> skillButtons;
 
-    private GamePause gamePause;
-
     // Start is called before the first frame update
     void Start()
     {
-        gamePause = GetComponent<GamePause>();
-
         skillInstances.Items.Clear();
         foreach (SkillBase skillPrefab in skillPrefabs.Items)
         {
@@ -32,36 +30,60 @@ public class UpgradeMenuManager : MonoBehaviour
 
     public void OpenMenu()
     {
-        if (isEnabled)
+        lock (upgradeVariablesLock)
         {
-            //gamePause.Pause();
-            panel.SetActive(true);
-            bool isFirstButton = true;
-            List<SkillBase> selectedSkills = skillInstances.Items.Where(s => s.IsUpgradable()).ToList();
-            for (int i = 0; i < skillButtons.Count; i++)
+            pendingUpgradeCount.ApplyChange(1);
+            if (panel.activeInHierarchy)
             {
-                SkillUpgradeButton skillButton = skillButtons[i];
-                if (i < selectedSkills.Count)
-                {
-                    skillButton.Enable();
-                    skillButton.SetSkill(selectedSkills[i]);
-                    if (isFirstButton)
-                    {
-                        isFirstButton = false;
-                        EventSystem.current.SetSelectedGameObject(skillButtons[0].gameObject);
-                    }
-                }
-                else
-                {
-                    skillButton.Disable();
-                }
+                // There is another upgrade waiting for user to select.
+                return;
             }
+
+            panel.SetActive(true);
         }
+
+        RefreshMenu();
     }
 
     public void CloseMenu()
     {
-        gamePause.Unpause();
-        panel.SetActive(false);
+        lock (upgradeVariablesLock)
+        {
+            pendingUpgradeCount.ApplyChange(-1);
+            if (pendingUpgradeCount.value <= 0)
+            {
+                panel.SetActive(false);
+                return;
+            }
+        }
+
+        // Still have pending upgrades.
+        RefreshMenu();
+    }
+
+    private void RefreshMenu()
+    {
+        bool isFirstButton = true;
+        List<SkillBase> selectedSkills = skillInstances.Items.Where(s => s.IsUpgradable()).ToList();
+        for (int i = 0; i < skillButtons.Count; i++)
+        {
+            SkillUpgradeButton skillButton = skillButtons[i];
+            if (i < selectedSkills.Count)
+            {
+                skillButton.Enable();
+                skillButton.SetSkill(selectedSkills[i]);
+                if (isFirstButton)
+                {
+                    isFirstButton = false;
+                    EventSystem.current.SetSelectedGameObject(skillButtons[0].gameObject);
+                }
+            }
+            else
+            {
+                skillButton.Disable();
+            }
+        }
+
+        // Invoke event
     }
 }
