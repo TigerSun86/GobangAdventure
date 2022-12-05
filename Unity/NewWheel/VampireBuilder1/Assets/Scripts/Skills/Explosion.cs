67e9b39452548dc2713c36a55f40e4b921ceafc2
Tiger Sun <tigersun86@gmail.com>
2022-12-04 21:32:33 -0800

    Split Explosion's skill and functionality into separate classes

---
+++ Unity/NewWheel/VampireBuilder1/Assets/Scripts/Skills/Explosion.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Explosion : MonoBehaviour
+{
+    // Used to display Gizmos
+    private static readonly float TIME_TO_LIVE = 1f;
+
+    [SerializeField] FloatVariable attackAreaFactor;
+
+    [SerializeField] AttackData attackBase = new AttackData(0.5f);
+
+    [SerializeField] GameObject effect;
+
+    [SerializeField] UnityEvent<GameObject, AttackData> attackTargetSelectEvent;
+
+    float radius;
+
+    float timer;
+
+    private void Start()
+    {
+        timer = TIME_TO_LIVE;
+        radius = attackAreaFactor.value;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(this.gameObject.transform.position, radius);
+        foreach (Collider2D collider in colliders)
+        {
+            if (DamagableUtilities.IsDamagableEnemy(collider.gameObject))
+            {
+                attackTargetSelectEvent.Invoke(collider.gameObject, attackBase);
+            }
+        }
+
+        if (effect != null)
+        {
+            GameObject effectInstance = Instantiate(effect, this.gameObject.transform.position, Quaternion.identity, this.transform);
+            effectInstance.transform.localScale *= attackAreaFactor.value * 2;
+        }
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.gameObject.transform.position, radius);
+    }
+}
--- Unity/NewWheel/VampireBuilder1/Assets/Scripts/Skills/ExplosionSkill.cs
+++ Unity/NewWheel/VampireBuilder1/Assets/Scripts/Skills/ExplosionSkill.cs
@@ -1,23 +1,8 @@
 using UnityEngine;
-using UnityEngine.Events;
 
 public class ExplosionSkill : SkillBase
 {
-    [SerializeField] FloatVariable attackFactor;
-
-    [SerializeField] FloatVariable attackAreaFactor;
-
-    [SerializeField] CriticalHit criticalHit;
-
-    [SerializeField] GameObject effect;
-
-    [SerializeField] UnityEvent<GameObject, AttackData> attackTargetSelectEvent;
-
-    [SerializeField] AttackData attackBase = new AttackData(0.5f);
-
-    float radius;
-
-    private Vector2? debugPosition;
+    [SerializeField] GameObject prefab;
 
     public override void LevelUp()
     {
@@ -41,35 +26,10 @@ public class ExplosionSkill : SkillBase
             return;
         }
 
-        float attack = (int)(attackFactor.value / 2);
-        debugPosition = bullet.transform.position;
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(bullet.transform.position, radius);
-        foreach (Collider2D collider in colliders)
-        {
-            if (DamagableUtilities.IsDamagableEnemy(collider.gameObject))
-            {
-                attackTargetSelectEvent.Invoke(collider.gameObject, attackBase);
-            }
-        }
-
-        if (effect != null)
-        {
-            GameObject effectInstance = Instantiate(effect, bullet.transform.position, Quaternion.identity, this.transform);
-            effectInstance.transform.localScale *= attackAreaFactor.value * 2;
-        }
-    }
-
-    private void Start()
-    {
-        radius = attackAreaFactor.value + 1;
-    }
-
-    void OnDrawGizmos()
-    {
-        if (debugPosition.HasValue)
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(debugPosition.Value, radius);
-        }
+        Instantiate(
+            prefab,
+            bullet.transform.position,
+            Quaternion.identity,
+            this.transform);
     }
 }
