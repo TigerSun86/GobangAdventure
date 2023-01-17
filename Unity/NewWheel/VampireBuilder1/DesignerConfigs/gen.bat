804656fd3b5911e060bdaed52be41baf3c068176
Tiger Sun <tigersun86@gmail.com>
2023-01-16 22:56:39 -0800

    Used Luban lib for designer configs management

---
+++ Unity/NewWheel/VampireBuilder1/DesignerConfigs/gen.bat
@@ -0,0 +1,13 @@
+set WORKSPACE=..
+
+set GEN_CLIENT=%WORKSPACE%\DesignerConfigs\Luban.ClientServer\Luban.ClientServer.exe
+set CONF_ROOT=%WORKSPACE%\DesignerConfigs
+
+%GEN_CLIENT% -j cfg --^
+ -d %CONF_ROOT%\Defines\__root__.xml ^
+ --input_data_dir %CONF_ROOT%\Datas ^
+ --output_data_dir ..\GenerateDatas\json ^
+ --gen_types data_json ^
+ -s all 
+
+pause
