039f27d78f9337ef748a387e942c3ac80b804547
“Tiger <tigersun86@gmail.com>
2023-02-12 22:10:33 -0800

    Fixed bug gen.sh cannot run

--- Unity/NewWheel/VampireBuilder1/DesignerConfigs/gen.sh
+++ Unity/NewWheel/VampireBuilder1/DesignerConfigs/gen.sh
@@ -1,13 +1,2 @@
-#!/bin/zsh
-WORKSPACE=..
-
-GEN_CLIENT=${WORKSPACE}/DesignerConfigs/Luban.ClientServer/Luban.ClientServer.dll
-CONF_ROOT=${WORKSPACE}/DesignerConfigs
-
-
-dotnet ${GEN_CLIENT} -j cfg --\
- -d ${CONF_ROOT}/Defines/__root__.xml \
- --input_data_dir ${CONF_ROOT}/Datas \
- --output_data_dir ${CONF_ROOT}/GenerateDatas/json \
- --gen_types data_json \
- -s all 
+#!/bin/zsh 
+dotnet ./Luban.ClientServer/Luban.ClientServer.dll  -j cfg --  -d ./Defines/__root__.xml  --input_data_dir ./Datas  --output_data_dir ./GenerateDatas/json   --gen_types data_json   -s all  
